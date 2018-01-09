e8f7733065e9afa55392af5aad29e5399a429c8d
Eric Liang <liang.eric24@gmail.com>
2018-01-09 10:54:06 -0800

    cleaned link colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 links = document.querySelectorAll(".ga-link");
-links.forEach(function (element) {
-  element.addEventListener("click", function() {
-    var name = element.getAttribute("ga-name");
+links.forEach(function (el) {
+  el.addEventListener("click", function() {
+    var name = el.getAttribute("ga-name");
     ga('send','event','Outbound Link','click', name);
   })
 });
+
+resumeURLs = document.querySelectorAll(".resume-url-target");
+resumeURLs.forEach(function (el) {
+	el.setAttribute("href", "https://www.dropbox.com/s/s9vti89m8sfnsi1/eric_liang_resume.pdf?dl=0");
+})
