f1897f2c13425afc94bf9798f7592fd5f427c095
Eric Liang <liang.eric24@gmail.com>
2018-09-18 00:10:55 -0700

    redesign 2018

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-links = document.querySelectorAll(".ga-link");
-links.forEach(function (el) {
-  el.addEventListener("click", function() {
-    var name = el.getAttribute("ga-name");
-    ga('send','event','Outbound Link','click', name);
-  })
-});
